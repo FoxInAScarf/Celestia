d8d8566f634c4a70f30c11b83be076ab8e6101d7
foxinascarf <veoacc2@gmail.com>
2022-09-20 16:19:00 +0200

    Moved to maven!

---
+++ src/veo/game/custom/gens/Flag.java
@@ -0,0 +1,123 @@
+package com.celestia.veo.game.custom.gens;
+
+import com.celestia.veo.Main;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy;
+import org.bukkit.*;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class Flag {
+
+    public String name;
+    public OfflinePlayer owner;
+    public Location head, pole;
+    private ArmorStand a, b, c;
+
+    public Flag(String name, Location head, Location pole) {
+
+        // generate blocks
+
+        head.getBlock().setType(Material.RED_WOOL);
+        for (int i = 0; i <= 9; i++)
+            pole.add(0, i, 0).getBlock().setType(Material.OAK_FENCE);
+        pole.add(0, 10, 0).getBlock().setType(Material.DARK_OAK_SLAB);
+
+        this.head = head;
+        this.pole = pole;
+        this.name = name;
+
+        unclaim();
+
+    }
+
+    public void claim(Player p) {
+
+        owner = p;
+        Main.sendMessage(p, ChatColor.GREEN + "You claimed the island of "
+                + name, false);
+        /*
+        *
+        *
+        * ADD CLAIM STATUE
+        *
+        *
+        * */
+        p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 3);
+
+        a.remove();
+        a = (ArmorStand) p.getWorld().spawnEntity(
+                new Location(head.getWorld(), head.getX(), head.getY() + 2, head.getZ()),
+                EntityType.ARMOR_STAND);
+        a.setMarker(true);
+        a.setInvisible(true);
+        a.setGravity(false);
+        a.setCustomName(ChatColor.RED + p.getName() + " has claimed this island!");
+        a.setCustomNameVisible(true);
+        a.addScoreboardTag("removable");
+
+        b = (ArmorStand) p.getWorld().spawnEntity(
+                new Location(head.getWorld(), head.getX(), head.getY() + 2.2, head.getZ()),
+                EntityType.ARMOR_STAND);
+        b.setMarker(true);
+        b.setInvisible(true);
+        b.setGravity(false);
+        b.setCustomName(ChatColor.GREEN + "Crouch here to tear the flag down.");
+        b.setCustomNameVisible(false);
+        b.addScoreboardTag("removable");
+
+        ((CraftPlayer) p).getHandle().b.a(new PacketPlayOutEntityDestroy(a.getEntityId()));
+        ((CraftPlayer) p).getHandle().b.a(new PacketPlayOutEntityDestroy(b.getEntityId()));
+
+        c = (ArmorStand) p.getWorld().spawnEntity(
+                new Location(head.getWorld(), head.getX(), head.getY() + 2, head.getZ()),
+                EntityType.ARMOR_STAND);
+        b.setMarker(true);
+        b.setInvisible(true);
+        b.setGravity(false);
+        b.setCustomName(ChatColor.GREEN + "You've claimed this island!");
+        b.setCustomNameVisible(false);
+        c.addScoreboardTag("removable");
+
+        for (Player pl : Bukkit.getOnlinePlayers()) {
+
+            if (pl.equals(p)) continue;
+            ((CraftPlayer) pl).getHandle().b.a(new PacketPlayOutEntityDestroy(c.getEntityId()));
+
+        }
+
+    }
+
+    public void unclaim() {
+
+        owner = null;
+        a.remove();
+        b.remove();
+        c.remove();
+        /*for (Player p : Bukkit.getOnlinePlayers())
+            Main.sendMessage(p, ChatColor.GREEN + "The island of " + name
+                    + " has been unclaimed!", false);*/
+        a = (ArmorStand) head.getWorld().spawnEntity(
+                new Location(head.getWorld(), head.getX(), head.getY() + 0.3, head.getZ()),
+                EntityType.ARMOR_STAND);
+        a.setMarker(true);
+        a.setInvisible(true);
+        a.setGravity(false);
+        a.setCustomName(ChatColor.GREEN + "Click here to claimed this island!");
+        a.setCustomNameVisible(true);
+        a.addScoreboardTag("removable");
+
+    }
+
+    public void remove() {
+
+        unclaim();
+        GenManager.flags.remove(this);
+        GenManager.flagFile.removeLine(name + "@" + head.getWorld() + "@" + head.getX() +
+                "@" + head.getY() + "@" + head.getZ() + "@"
+                + pole.getX() + "@" + pole.getY() + "@" + pole.getZ() + "@");
+
+    }
+
+}
---
+++ src/veo/game/custom/gens/Generator.java
@@ -0,0 +1,92 @@
+package com.celestia.veo.game.custom.gens;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+public class Generator {
+
+    public String name;
+    Material m;
+    Location l;
+    int time = 0, length;
+    ArmorStand s;
+    ArmorStand n;
+
+    public Generator(String name, Material m, Location l, int length) {
+
+        this.name = name;
+        this.m = m;
+        this.l = l;
+        this.length = length;
+        s = (ArmorStand) l.getWorld().spawnEntity(
+                new Location(l.getWorld(), l.getX(), l.getY() + 2, l.getZ()), EntityType.ARMOR_STAND);
+        s.setInvisible(true);
+        s.setMarker(true);
+        s.setGravity(false);
+        s.setHelmet(new ItemStack(m));
+        s.addScoreboardTag("removable");
+
+        n = (ArmorStand) l.getWorld().spawnEntity(
+                new Location(l.getWorld(), l.getX(), l.getY() + 4.2, l.getZ()), EntityType.ARMOR_STAND);
+        n.setCustomName(ChatColor.GREEN + "Next item is in " + ChatColor.GOLD
+                + (length - time) + " seconds...");
+        n.setCustomNameVisible(true);
+        n.setInvisible(true);
+        n.setMarker(true);
+        n.setGravity(false);
+        n.addScoreboardTag("removable");
+
+    }
+
+    public void run() {
+
+        Location tl = s.getLocation();
+        tl.setYaw(tl.getYaw() + 5);
+        s.teleport(tl);
+
+        for (Player p : Bukkit.getOnlinePlayers())
+            if (p.getLocation().distance(s.getLocation()) >= 4) {
+
+                n.setCustomName(ChatColor.GREEN + "Stand here!");
+                time = 0;
+                return;
+
+            }
+
+        if (time == length) {
+
+            time = 0;
+            Item i = l.getWorld().dropItemNaturally(new Location(l.getWorld(),
+                            l.getX(), l.getY() + 2, l.getZ()),
+                    new ItemStack(m));
+            i.teleport(new Location(l.getWorld(), s.getLocation().getX(),
+                    s.getLocation().getY() + 1.8, s.getLocation().getZ()));
+            i.setVelocity(new Vector(0, 0, 0));
+
+
+        } else time++;
+
+        if (time % 20 == 0) n.setCustomName(ChatColor.GREEN + "Next item is in " + ChatColor.GOLD
+                + (length - time) / 20 + " seconds...");
+
+    }
+
+    public void remove() {
+
+        s.remove();
+        n.remove();
+        GenManager.gens.remove(this);
+        GenManager.genFile.removeLine(name + "@" + m.toString() + "@" + l.getWorld() + "@"
+                + l.getX() + "@" + l.getY() + "@" + l.getZ() + "@" + time);
+
+    }
+
+}
