d57cf01762d500d2f7a0908c59471bd1725c01bd
foxinascarf <veoacc2@gmail.com>
2022-10-13 21:52:36 +0200

    Saving Shop #4

--- src/veo/game/shop/Shop.java
+++ src/veo/game/shop/Shop.java
@@ -1,17 +1,20 @@
 package veo.game.shop;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import veo.essentials.zfm.ZFile;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 
 public class Shop extends ZFile {
 
@@ -129,14 +132,79 @@ public class Shop extends ZFile {
 
         }
 
-        /*
-        *
-        *
-        * ADD SCROLLABLE PAGES
-        * AND STUFF
-        *
-        *
-        * */
+        // stupid gray shit
+        for (int i = 0; i <= 8; i++) inv.setItem(i, grayPane);
+        for (int i = 9; i <= 17; i++) inv.setItem(i, grayPane);
+        for (int i = 9; i <= 36; i += 9) inv.setItem(i, grayPane);
+        for (int i = 18; i <= 45; i += 9) inv.setItem(i - 1, grayPane);
+        for (int i = 18; i <= 45; i += 9) inv.setItem(i - 3, grayPane);
+        for (int i = 45; i <= 53; i++) inv.setItem(i, grayPane);
+
+        // ADD ARROWS
+        ItemStack left = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta lsMeta = (SkullMeta) left.getItemMeta();
+        GameProfile lsProfile = new GameProfile(UUID.randomUUID(), null);
+        lsProfile.getProperties().put("textures", new Property("textures", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTQyZmRlOGI4MmU4YzFiOGMyMmIyMjY3OTk4M2ZlMzVjYjc2YTc5Nzc4NDI5YmRhZGFiYzM5N2ZkMTUwNjEifX19"));
+        Field lsField;
+        try {
+
+            lsField = lsMeta.getClass().getDeclaredField("profile");
+            lsField.setAccessible(true);
+            lsField.set(lsMeta, lsProfile);
+
+        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e)
+        { e.printStackTrace(); }
+        lsMeta.setDisplayName(ChatColor.GREEN + "<- Scroll left");
+        left.setItemMeta(lsMeta);
+
+        inv.setItem(0, left);
+
+        ItemStack right = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta rsMeta = (SkullMeta) right.getItemMeta();
+        GameProfile rsProfile = new GameProfile(UUID.randomUUID(), null);
+        rsProfile.getProperties().put("textures", new Property("textures", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDA2MjYyYWYxZDVmNDE0YzU5NzA1NWMyMmUzOWNjZTE0OGU1ZWRiZWM0NTU1OWEyZDZiODhjOGQ2N2I5MmVhNiJ9fX0="));
+        Field rsField;
+        try {
+
+            rsField = rsMeta.getClass().getDeclaredField("profile");
+            rsField.setAccessible(true);
+            rsField.set(rsMeta, rsProfile);
+
+        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e)
+        { e.printStackTrace(); }
+        rsMeta.setDisplayName(ChatColor.GREEN + "Scroll right ->");
+        right.setItemMeta(rsMeta);
+
+        inv.setItem(8, right);
+
+        // ADD PAGES
+        for (int i = 0; i <= pages - 1; i++) {
+
+            ItemStack page = new ItemStack(currentPage == pages + 1 ? Material.RED_STAINED_GLASS_PANE
+                    : Material.LIME_STAINED_GLASS_PANE);
+            ItemMeta pageMeta = page.getItemMeta();
+            if (currentPage == pages + 1) {
+
+                pageMeta.setDisplayName(ChatColor.RED + "Page #" + (i + 1));
+                pageMeta.setLore(Arrays.asList(ChatColor.GRAY + "Selected"));
+
+            }
+            else pageMeta.setDisplayName(ChatColor.GREEN + "Page #" + (i + 1));
+
+            inv.setItem(2 + i, page);
+
+        }
+
+        // ADD ITEMS
+        for (int i = 0; i <= recipes.size() - 1; i++) {
+
+            Recipe r = recipes.get(i);
+            inv.setItem(25 + (9 * i), r.item);
+            for (int j = 0; j <= r.elements.size() - 1; j++)
+                inv.setItem((25 + (9 * i)) - (2 + j), r.elements.get(j));
+
+        }
+
         p.openInventory(inv);
 
     }
